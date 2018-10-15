b5c031b6b4cd8516ec1f620ae43e52decd5edfcd
Jules Stoltz <julesstoltz@gmail.com>
2018-10-15 15:04:47 -0400

    created MyLinkedList project files

---
+++ MyLinkedList/MyLinkedListUT/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyLinkedListUT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyLinkedListUT")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a9445f48-0a66-4219-b3c0-35d0c13b6e42")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyLinkedList/MyLinkedListUT/TestMyLL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLinkedListUT
+{
+    [TestClass]
+    public class TestMyLL
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
